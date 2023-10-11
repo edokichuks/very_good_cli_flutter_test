edaa378bdea061deaa5400ab8f00a01e0979fe9f
Edoki Chukwuyem <edokichuks@gmail.com>
2023-10-11 15:49:33 +0100

    very good cli setup is great

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.dummy_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:dummy_project/app/app.dart';
+import 'package:dummy_project/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
